97bfd960020efc1fc8426de0a29caffca63d0023
miguel9177 <luis2.128.b80@gmail.com>
2023-03-28 00:46:38 +0100

    imported enet, started client connection

--- 6020CEM Physics with Box2D/Framework/Framework/AllUserScipts/SB_MultiplayerServerClient.cpp
+++ 6020CEM Physics with Box2D/Framework/Framework/AllUserScipts/SB_MultiplayerServerClient.cpp
@@ -1,12 +1,64 @@
 #include "SB_MultiplayerServerClient.h"
+#include <iostream>
+
 
 SB_MultiplayerServerClient::SB_MultiplayerServerClient() : ScriptBehaviour(uniqueComponentIdIdentifier)
 {
+	address = new ENetAddress();
+	client = new ENetHost();
+	peer = new ENetPeer();
+	serverData = new PhysicsData;
+	clientData = new ClientData;
+	clientPacket = new ClientPacket;
+	dataPacket = new ENetPacket();
+	enetEvent = new ENetEvent();
+}
+
+SB_MultiplayerServerClient::~SB_MultiplayerServerClient()
+{
+	if (peer != NULL)
+	{
+		enet_peer_disconnect_now(peer, 0);
+	}
+
+	delete client;
+	delete peer;
+	delete serverData;
+	delete clientData;
+	delete dataPacket;
 }
 
 void SB_MultiplayerServerClient::Start()
 {
+	if (enet_initialize() != 0)
+	{
+		std::cout << "Enet failed to initialise!" << "\n\n" << std::endl;
+	}
 
+	client = enet_host_create(NULL, 1, 2, 0, 0);
+
+	if (client == NULL)
+	{
+		std::cout << "Client failed to initialise!" << "\n\n" << std::endl;
+	}
+
+	enet_address_set_host(address, "localhost");
+	address->port = 1234;
+
+	peer = enet_host_connect(client, address, 2, 0);
+
+	if (peer == NULL) {
+		std::cout << "No available peers for initializing an ENet connection.\n" << std::endl;
+	}
+
+
+	int* packetType = new int;
+
+	*packetType = -1;
+
+
+
+	int clientIndex = -1;
 }
 
 void SB_MultiplayerServerClient::Update()
--- 6020CEM Physics with Box2D/Framework/Framework/AllUserScipts/SB_MultiplayerServerClient.h
+++ 6020CEM Physics with Box2D/Framework/Framework/AllUserScipts/SB_MultiplayerServerClient.h
@@ -1,15 +1,41 @@
 #pragma once
 #include "../ScriptBehaviour.h"
 #include "../Vector2.h"
+#include <enet/enet.h>
 
 class SB_MultiplayerServerClient : public ScriptBehaviour
 {
 	static const unsigned int uniqueComponentIdIdentifier = 10004;
 	
+	struct PhysicsData {
+		int packetType = 1;
+		Vector2 positions[2];
+	};
+
+	struct ClientData {
+		int packetType = 0;
+		int clientIndex;
+	};
+
+	struct ClientPacket {
+		int clientIndex;
+		Vector2 position;
+	};
+
+	ENetAddress* address;
+	ENetHost* client;
+	ENetPeer* peer;
+	PhysicsData* serverData;
+	ClientData* clientData;
+	ClientPacket* clientPacket;
+	ENetPacket* dataPacket;
+	ENetEvent* enetEvent;
+
 public:
 #pragma region Engine Functions
 
 	SB_MultiplayerServerClient();
+	~SB_MultiplayerServerClient();
 	void Start() override;
 	void Update() override;
 
